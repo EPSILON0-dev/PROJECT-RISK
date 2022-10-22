77299d1ace641e4b211770364466550ee0e90155
EPSILON0-dev <lforenc@wp.pl>
2022-10-22 12:14:47 +0200

    huj

--- hdl/peripheral/uart/tx.v
+++ hdl/peripheral/uart/tx.v
@@ -1,8 +1,19 @@
 module uart_tx(
-  input i_clk,
-  input i_ce,
-  input i_rst,
-  input [8:0] i_data,
-  input       i_2stop,
+  input       i_clk,
+  input       i_ce,
+  input       i_rst,
 
-)
+  input [8:0] i_data,
+  input [1:0] i_length,
+  input       i_2stop,
+  input       i_parity,
+  input       i_odd,
+  input       i_start,
+
+  output      o_tx,
+  output      o_busy
+);
+
+
+
+endmodule
